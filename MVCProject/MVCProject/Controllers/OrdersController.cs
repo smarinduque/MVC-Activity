89997cc5b1498cf95e0b3df92f5b518f58177ab0
smarinduque <s.marinduque@bpsolutions.biz>
2022-12-15 10:55:51 +0800

    BPS0053 Add Orders Form

---
+++ MVCProject/MVCProject/Controllers/OrdersController.cs
@@ -0,0 +1,33 @@
+﻿using MVCProject.Context;
+using MVCProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCProject.Controllers
+{
+    public class OrdersController : Controller
+    {
+        CustomerContext cc = new CustomerContext();
+        // GET: Orders
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Order()
+        {
+            var customers = cc.Customers.ToList();
+            var model = customers.Select(c => new CustomerDTO
+            {
+                Id = c.Id,
+                ContactName = c.ContactName
+            }).ToList();
+
+            SelectList list = new SelectList(model, "Id", "ContactName");
+            ViewData["CustomerList"] = list;
+            return View();
+        }
+    }
+}
