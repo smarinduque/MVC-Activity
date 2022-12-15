093acf3e3b1542c769578c1f51db2f0d77a64abd
smarinduque <s.marinduque@bpsolutions.biz>
2022-12-15 09:51:42 +0800

    Add Order Form

---
+++ MVCProject/MVCProject/Controllers/OrdersController.cs
@@ -0,0 +1,41 @@
+﻿using MVCProject.Context;
+using MVCProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCProject.Controllers
+{
+    public class OrdersController : Controller
+    {
+        CustomerContext cc = new CustomerContext();
+        // GET: Orders
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //public ActionResult Order(String orderno, String orderdate, String customername, String totalamount) 
+        //{
+        //    ViewBag.on = string.Format("{0}", orderno);
+        //    ViewBag.od = orderdate;
+        //    ViewBag.cn = customername;
+        //    ViewBag.ta = totalamount;
+        //    return View();
+        //}
+        public ActionResult Order() 
+        {
+            var customers = cc.Customers.ToList();
+            var model = customers.Select(c => new CustomerDTO
+            {
+                Id = c.Id,
+                ContactName = c.ContactName
+            }).ToList();
+
+            SelectList list = new SelectList(model, "Id", "ContactName");
+            ViewData["CustomerList"] = list;
+            return View();
+        }
+    }
+}
